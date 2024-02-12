1add6e6ea1caabf9737be76912e5a4a3116a342e
Arvid Dawn Amin <bahalul1983@outlook.com>
2024-02-12 20:46:40 +0100

    HTML COMPLEATE FOR WHOLE APPLICATION

--- src/components/main/section/Resume.jsx
+++ src/components/main/section/Resume.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import HeaderPage from "../../header/HeaderPage";
-
+import cvOne from "../../../assets/cv&coverLetter/CV2024-1.jpg";
+import cvTwo from "../../../assets/cv&coverLetter/CV2024-2.jpg";
+import coverLetter from "../../../assets/cv&coverLetter/Personligbrev.jpg";
 function Resume() {
   return (
-    <div>
-      <HeaderPage />
-      <h1>Resume</h1>
-    </div>
+    <>
+      <div>
+        <HeaderPage />
+      </div>
+
+      <div className="cv-cover-letter">
+        <div className="cv-section">
+          <div className="cvOne">
+            <img src={cvOne} alt="cvOne" />
+          </div>
+          <div className="cvTwo">
+            <img src={cvTwo} alt="cvTwo" />
+          </div>
+        </div>
+        <div className="cover-letter">
+          <img src={coverLetter} alt="coverLatter" />
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/components/main/section/Skills.jsx
+++ src/components/main/section/Skills.jsx
@@ -1,14 +1,72 @@
 import React from "react";
 import HeaderPage from "../../header/HeaderPage";
+import "./Skills.css"; // Import CSS file for styling
 
 function Skills() {
   return (
-    <div style={{ display: "flex" }}>
-      <HeaderPage />
+    <>
       <div>
-        <h1>Skills</h1>
+        <HeaderPage />
       </div>
-    </div>
+
+      <div>
+        <h2>
+          Passionerad Programmerare med Erfarenhet inom Fullstack Utveckling
+        </h2>
+        <div className="skill">
+          <h4>C#</h4>
+          <div className="bar" style={{ width: "80%" }}></div>
+        </div>
+        <div className="skill">
+          <h4>MS SQL</h4>
+          <div className="bar" style={{ width: "85%" }}></div>
+        </div>
+        <div className="skill">
+          <h4>ASP.NET Core MVC & Razor Pages</h4>
+          <div className="bar" style={{ width: "70%" }}></div>
+        </div>
+        <div className="skill">
+          <h4>JavaScript</h4>
+          <div className="bar" style={{ width: "75%" }}></div>
+        </div>
+        <div className="skill">
+          <h4>React</h4>
+          <div className="bar" style={{ width: "80%" }}></div>
+        </div>
+        <div className="skill">
+          <h4>Redux Toolkit</h4>
+          <div className="bar" style={{ width: "90%" }}></div>
+        </div>
+        <div className="skill">
+          <h4>HTML5</h4>
+          <div className="bar" style={{ width: "95%" }}></div>
+        </div>
+        <div className="skill">
+          <h4>CSS3 and SCSS</h4>
+          <div className="bar" style={{ width: "98%" }}></div>
+        </div>
+        <div className="skill">
+          <h4>Bootstrap</h4>
+          <div className="bar" style={{ width: "70%" }}></div>
+        </div>
+        <div className="skill">
+          <h4>XD, Figma</h4>
+          <div className="bar" style={{ width: "75%" }}></div>
+        </div>
+        <div className="skill">
+          <h4>JIRA</h4>
+          <div className="bar" style={{ width: "90%" }}></div>
+        </div>
+        <div className="skill">
+          <h4>GIT och GitHub</h4>
+          <div className="bar" style={{ width: "95%" }}></div>
+        </div>
+        <div className="skill">
+          <h4>IT SUPPORT TEKNIKARE (Google certification)</h4>
+          <div className="bar" style={{ width: "70%" }}></div>
+        </div>
+      </div>
+    </>
   );
 }
 
