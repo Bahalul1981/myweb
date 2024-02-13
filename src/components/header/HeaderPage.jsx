6803fd512e49114b2c443f0a22428159a30576ad
Arvid Dawn Amin <bahalul1983@outlook.com>
2024-02-13 15:34:27 +0100

    working for header

--- src/components/header/HeaderPage.jsx
+++ src/components/header/HeaderPage.jsx
@@ -10,36 +10,80 @@ function HeaderPage() {
           <div className="logo-image">
             <img src={logoImg} alt="logo" className="my-logo-image" />
           </div>
-          <div className="logoText"></div>
-          <div className="mediaIcons"></div>
+          <div className="logoText">
+            <h1>Arvid dawn</h1>
+          </div>
+          <div className="mediaIcons">
+            <a href="">
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="">
+              <i class="fa-brands fa-github"></i>
+            </a>
+            <a href="">
+              <i class="fa-brands fa-facebook"></i>
+            </a>
+            <a href="">
+              <i class="fa-brands fa-youtube"></i>
+            </a>
+            <a href="">
+              <i class="fa-brands fa-instagram"></i>
+            </a>
+          </div>
         </div>
         <nav className="header-nav">
           <ul>
             <li>
-              <Link to="/">Hem</Link>
+              <Link to="/">
+                <Icon icon="home" /> Hem
+              </Link>
             </li>
             <li>
-              <Link to="/about">Om mig</Link>
+              <Link to="/about">
+                {" "}
+                <Icon icon="person" /> Om mig
+              </Link>
             </li>
             <li>
-              <Link to="/contact">Kontakt</Link>
+              <Link to="/contact">
+                {" "}
+                <Icon icon="inbox" /> Kontakt
+              </Link>
             </li>
             <li>
-              <Link to="/project">Projekt</Link>
+              <Link to="/project">
+                {" "}
+                <Icon icon="projects" /> Projekt
+              </Link>
             </li>
             <li>
-              <Link to="/resum">CV</Link>
+              <Link to="/resum">
+                {" "}
+                <Icon icon="more" /> CV
+              </Link>
             </li>
             <li>
-              <Link to="/skills">Kompetens</Link>
+              <Link to="/skills">
+                {" "}
+                <Icon icon="fast-forward" /> Kompetens
+              </Link>
             </li>
           </ul>
         </nav>
         <p className="header-p">
           &copy; {new Date().getFullYear()}. All rights reserved to Arvid.
         </p>
-        <div className="Header-burger-menue">
-          <Icon icon="menu" />
+        <div className="responsive">
+          <div className="mobile-res">
+            <i class="fa-solid fa-mobile-screen-button"></i>
+          </div>
+          <div className="dekstop-res">
+            <i class="fa-sharp fa-solid fa-desktop"></i>
+          </div>
+        </div>
+        <div className="Header-burger-cross-menue">
+          <Icon icon="menu" className="burger" />
+          <Icon icon="cross" className="cross-menue" />
         </div>
       </header>
     </div>
