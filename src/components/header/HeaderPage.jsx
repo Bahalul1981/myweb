3adc66da5c42075ff44cfea207bdcd129e830d71
Arvid Dawn Amin <bahalul1983@outlook.com>
2024-02-13 22:03:33 +0100

    more style added

--- src/components/header/HeaderPage.jsx
+++ src/components/header/HeaderPage.jsx
@@ -9,6 +9,10 @@ function HeaderPage() {
         <div className="header-image-and-tex">
           <div className="logo-image">
             <img src={logoImg} alt="logo" className="my-logo-image" />
+            <div className="Header-burger-cross-menue">
+              <Icon icon="menu" className="burger" />
+              <Icon icon="cross" className="cross-menue" />
+            </div>
           </div>
           <div className="logoText">
             <h1>Arvid dawn</h1>
@@ -31,45 +35,47 @@ function HeaderPage() {
             </a>
           </div>
         </div>
-        <nav className="header-nav">
-          <ul>
-            <li>
-              <Link to="/">
-                <Icon icon="home" /> Hem
-              </Link>
-            </li>
-            <li>
-              <Link to="/about">
-                {" "}
-                <Icon icon="person" /> Om mig
-              </Link>
-            </li>
-            <li>
-              <Link to="/contact">
-                {" "}
-                <Icon icon="inbox" /> Kontakt
-              </Link>
-            </li>
-            <li>
-              <Link to="/project">
-                {" "}
-                <Icon icon="projects" /> Projekt
-              </Link>
-            </li>
-            <li>
-              <Link to="/resum">
-                {" "}
-                <Icon icon="more" /> CV
-              </Link>
-            </li>
-            <li>
-              <Link to="/skills">
-                {" "}
-                <Icon icon="fast-forward" /> Kompetens
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <div className="header-nav">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">
+                  <Icon icon="home" /> Hem
+                </Link>
+              </li>
+              <li>
+                <Link to="/about">
+                  {" "}
+                  <Icon icon="person" /> Om mig
+                </Link>
+              </li>
+              <li>
+                <Link to="/contact">
+                  {" "}
+                  <Icon icon="inbox" /> Kontakt
+                </Link>
+              </li>
+              <li>
+                <Link to="/project">
+                  {" "}
+                  <Icon icon="projects" /> Projekt
+                </Link>
+              </li>
+              <li>
+                <Link to="/resum">
+                  {" "}
+                  <Icon icon="more" /> CV
+                </Link>
+              </li>
+              <li>
+                <Link to="/skills">
+                  {" "}
+                  <Icon icon="fast-forward" /> Kompetens
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <p className="header-p">
           &copy; {new Date().getFullYear()}. All rights reserved to Arvid.
         </p>
@@ -80,10 +86,6 @@ function HeaderPage() {
           <div className="dekstop-res">
             <i class="fa-sharp fa-solid fa-desktop"></i>
           </div>
-        </div>
-        <div className="Header-burger-cross-menue">
-          <Icon icon="menu" className="burger" />
-          <Icon icon="cross" className="cross-menue" />
         </div>
       </header>
     </div>
