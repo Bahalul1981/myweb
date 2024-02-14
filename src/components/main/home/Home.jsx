80c3fa48d9f3ed6de20deb6862d06706ee419b0c
Arvid Dawn Amin <bahalul1983@outlook.com>
2024-02-14 01:00:07 +0100

    Main page is done

--- src/components/main/home/Home.jsx
+++ src/components/main/home/Home.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import HeaderPage from "../../header/HeaderPage";
 import useCurrentDateTime from "./CurrentDateTime";
-import HomeImage from "../../../assets/image/homepageImg.jpg";
-import video from "../../../assets/image/dawn.mp4";
+import video from "../../../assets/image/dawnamin.mp4";
 function Home() {
   const { currentDateTime, formatDateTime } = useCurrentDateTime();
 
@@ -11,31 +10,13 @@ function Home() {
       <div className="homepage-header-nav">
         <HeaderPage />
       </div>
+
       <div className="home-image-text">
-        <div className="homepage-text">
-          <h1>Arvid Dawn Amin</h1>
-          <h3>Jag är .NET Utvecklare</h3>
-          <h3>Jag är Front-End Utvecklare</h3>
-          <h3>Jag är Fullstack Utvecklare</h3>
-          <h3>Jag är IT supporttekniker</h3>
-          <h3>Jag är UX designer</h3>
-        </div>
-        <div className="home-imge">
-          <img
-            src={HomeImage}
-            alt="myImage"
-            style={{ maxWidth: "100%", width: "auto" }}
-          />
-          <video
-            src={video}
-            autoPlay
-            loop
-            muted
-            style={{ maxWidth: "100%", width: "auto" }}
-          ></video>
-        </div>
         <div className="current-time-and-date">
           <h3>{formatDateTime(currentDateTime)}</h3>
+        </div>
+        <div className="home-video">
+          <video src={video} autoPlay loop muted></video>
         </div>
       </div>
     </div>
